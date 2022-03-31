21ec59d4f65471c4f0f7e7343b5cd7999b3399ef
Abhishek Verma <abhi.verma96480@gmail.com>
2022-04-01 01:07:53 +0530

    :rocket: Post Login Algorithms Tab Opened

--- Hackerrank/Hackerank.js
+++ Hackerrank/Hackerank.js
@@ -1,48 +1,85 @@
 const puppeteer = require("puppeteer");
-const constants = require("./constants.js");
+const constants = require("../constants.js");
 let cTab;
 
 let browserOpenPromise = puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-    args: ["--start-maximized"],
-    // executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", // chrome://version
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+  // executablePath: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", // chrome://version
 });
-  
-// If BrowserOpenPromise resolves
+
+//If BrowserOpenPromise resolves, we get instance of browser
 //Promise Chaining
 browserOpenPromise
-.then(function(browser){
+  .then(function (browser) {
     console.log("Browser Opened");
     //We get Browser Obj if BrowserOpenPromise is resolved
     console.log("Browser", browser);
     let allTabsPromise = browser.pages();
     return allTabsPromise;
-})
-.then(function(allTabsArr){
+  })
+  .then(function (allTabsArr) {
     cTab = allTabsArr[0];
     console.log("New Tab");
 
     // URL to navigate page to
-    let visitingLoginPagePromise = cTab.goto("https://www.hackerrank.com/auth/login");
+    let visitingLoginPagePromise = cTab.goto(
+      "https://www.hackerrank.com/auth/login"
+    );
     return visitingLoginPagePromise;
-})
-.then(function() {
+  })
+  .then(function () {
     console.log("Opened Hackerrank Login Page");
-    let emailWillBeTypedPromise = cTab.type("input[id='input-1']", constants.email)
+    let emailWillBeTypedPromise = cTab.type(
+      "input[id='input-1']",
+      constants.email
+    );
     return emailWillBeTypedPromise;
-})
-.then(function(){
+  })
+  .then(function () {
     console.log("Email Typed Successfully");
-    let passwordWillBeTypedPromise = cTab.type("input[id='input-2']", constants.password);
+    let passwordWillBeTypedPromise = cTab.type(
+      "input[id='input-2']",
+      constants.password
+    );
     return passwordWillBeTypedPromise;
-})
-.then(function(){
+  })
+  .then(function () {
     console.log("Password Typed Successfully");
-    let LoginButtonClickedPromise = cTab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled")
+    let LoginButtonClickedPromise = cTab.click(
+      ".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled"
+    );
     return LoginButtonClickedPromise;
-})
-.then(function() {
+  })
+  .then(function () {
     console.log("Logged In Successfully");
-})
-.catch(function(err){console.log(err);})
+    // wait for selector will wait for the entire web page to load and it will find the needed element and then click the element/node.
+    let AlgoTabWillBeOpenedPromise = waitAndClick(
+      "div[data-automation='algorithms']"
+    );
+    return AlgoTabWillBeOpenedPromise;
+  })
+  .then(function () {
+    console.log("Algo Tab Opened");
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+function waitAndClick(algoBtn) {
+  let promise = new Promise((resolve, reject) => {
+    let waitForSelectorPromise = cTab.waitForSelector(algoBtn);
+    waitForSelectorPromise
+      .then(() => {
+        let clickPromise = cTab.click(algoBtn);
+        return clickPromise;
+      })
+      .then(() => resolve())
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+  promise.then(() => console.log("Wait and Click Promise Resolved!"))
+  return promise;
+}
